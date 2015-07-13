a4d4117e64d09c0ec56697a11491d00c005f2021
unknown <stawiarskiw@D2X6WZ4J.parliament.uk>
2015-07-13 14:31:42 +0100

    fix for released datasets workaround;

--- Scripts/modules/conductor.js
+++ Scripts/modules/conductor.js
@@ -76,7 +76,7 @@
         };
 
         self.getDataForEndpoints = function (data) {
-            if (self.genericUnit.host != "http://lda.data.parliament.uk")
+            if (self.genericUnit.host.indexOf("http://lda.data.parliament.uk")!=0)
                 releasedDatasets = "[]";
             var arr = endpointUnit.readEndpoints(data, JSON.parse(releasedDatasets));
             self.endpoints(arr);
